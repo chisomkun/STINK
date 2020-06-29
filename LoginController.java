6b1638f7de6696754fab5c4d3d388f451936d2e1
Tim Lewis <tlewis58@student.gsu.edu>
2020-06-28 22:08:49 -0400

    Add files via upload

---
+++ LoginController.java
@@ -0,0 +1,17 @@
+package com.helpdesk.helpdesk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/index")
+	public String login() {
+		return "index";
+		
+		
+		
+	}
+	
+}
