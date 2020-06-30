d1cf1160e99e1534fbf92589042a870bc89d0e96
Kevin Nong <Knong1@student.gsu.edu>
2020-06-30 16:40:07 -0400

    test

--- helpdesk/src/main/java/com/stinkelectronics/helpdesk/model/Account.java
+++ helpdesk/src/main/java/com/stinkelectronics/helpdesk/model/Account.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+//test
 @Entity
 @Table(name="Account")
 public class Account {
