39d83a0c0494001ef02c7024f80e9b8d51d9b048
Siva <smourougassamy1@student.gsu.edu>
2020-07-22 17:11:44 -0400

    Populate ProfileDao

--- helpdesk/src/main/java/service/ProfileDao.java
+++ helpdesk/src/main/java/service/ProfileDao.java
@@ -1,5 +1,58 @@
 package service;
 
-public class ProfileDao {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import com.stinkelectronics.helpdesk.model.Profile;
+
+@Repository
+@Configuration
+public class ProfileDao {
+	
+	@Autowired
+	private JdbcTemplate jtemp;
+	
+	//query by User ID (Unique)
+	public Profile getProfileByUserID(String uid) {
+		Profile profile = new Profile();
+		try {
+			String query = "SELECT * FROM Profile WHERE UserID=?";
+			return jtemp.queryForObject(query, new Object[]{uid}, new ProfileRowMapper());
+		}
+		catch(DataAccessException e) {
+			System.out.println(e.getMessage());
+			return profile;
+		}
+	}
+	
+	//ask about returning list for nonunique attribute
+	//query by Role ID (Non Unique)
+	public Profile getProfileByRoleID(int roleid) {
+		Profile profile = new Profile();
+		try {
+			String query = "SELECT * FROM Profile WHERE RoleID=?";
+			return jtemp.queryForObject(query, new Object[]{roleid}, new ProfileRowMapper());
+		}
+		catch(DataAccessException e) {
+			System.out.println(e.getMessage());
+			return profile;
+		}
+	}
+	
+	//ask about returning list for nonunique attribute
+	//query by Repair ID (Non Unique)
+	public Profile getProfileByRepairID(int repairid) {
+		Profile profile = new Profile();
+		try {
+			String query = "SELECT * FROM Profile WHERE RepairID=?";
+			return jtemp.queryForObject(query, new Object[]{repairid}, new ProfileRowMapper());
+		}
+		catch(DataAccessException e) {
+			System.out.println(e.getMessage());
+			return profile;
+		}
+	}
 }
