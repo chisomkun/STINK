cc40d8549aa4179291037df13e548fdbcbf8171e
Siva <smourougassamy1@student.gsu.edu>
2020-07-22 16:46:28 -0400

    Populate AccountRowMapper and RoleRowMapper

--- helpdesk/src/main/java/service/AccountRowMapper.java
+++ helpdesk/src/main/java/service/AccountRowMapper.java
@@ -1,5 +1,19 @@
 package service;
 
-public class AccountRowMapper {
+import java.sql.ResultSet;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.SQLException;
 
+import com.stinkelectronics.helpdesk.model.Account;
+
+public class AccountRowMapper implements RowMapper<Account> {
+	
+	@Override
+	public Account mapRow(ResultSet a, int rowA) throws SQLException {
+		Account acct = new Account();
+		acct.setEmail(a.getString("Email"));
+		acct.setPassword(a.getString("Password"));
+		acct.setUserID(a.getString("UserID"));
+		return acct;
+	}
 }
--- helpdesk/src/main/java/service/RoleRowMapper.java
+++ helpdesk/src/main/java/service/RoleRowMapper.java
@@ -1,5 +1,20 @@
 package service;
 
-public class RoleRowMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+
+import com.stinkelectronics.helpdesk.model.Role;
+
+public class RoleRowMapper implements RowMapper<Role>{
+	@Override
+	public Role mapRow(ResultSet set, int rnum) throws SQLException{
+		Role role = new Role();
+		
+		role.setRoleID(set.getInt("RoleID"));
+		role.setJob(set.getString("Job"));
+		
+		return role;
+	}
 }
