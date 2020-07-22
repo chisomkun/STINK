3f2e273cc60f0bf0290b6f6d5d5eb2b64deac82d
Kevin Nong <Knong1@student.gsu.edu>
2020-07-22 16:44:23 -0400

    Getters/Setters and Constructors

--- helpdesk/src/main/java/service/RepairRowMapper.java
+++ helpdesk/src/main/java/service/RepairRowMapper.java
@@ -1,17 +1,19 @@
 package service;
 
+import java.sql.ResultSet;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.SQLException;
 import com.stinkelectronics.helpdesk.model.Repair;
 
 public class RepairRowMapper implements RowMapper<Repair> {
 
     @Override
-
-    public Repair mapRow() {
+    public Repair mapRow(ResultSet rs, int rowNum) throws SQLException {
 
         Repair r = new Repair();
-        r.setEName();
-        r.setStatus();
-        r.setServiceID();
+        r.setEName(r.getEName("EName"));
+        r.setStatus(r.getStatus("Status"));
+        r.setServiceID(r.getServiceID("ServiceID"));
         return r;
     }
 }
