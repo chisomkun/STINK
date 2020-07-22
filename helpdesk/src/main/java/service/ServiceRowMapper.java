e816582a7747cda5983d6448e5bf398fbaf4df8e
Siva <smourougassamy1@student.gsu.edu>
2020-07-22 16:57:05 -0400

    Populate ServiceRowMapper

--- helpdesk/src/main/java/service/ServiceRowMapper.java
+++ helpdesk/src/main/java/service/ServiceRowMapper.java
@@ -1,5 +1,20 @@
 package service;
 
-public class ServiceRowMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+
+import com.stinkelectronics.helpdesk.model.Service;
+
+public class ServiceRowMapper implements RowMapper<Service> {
+
+	@Override
+	public Service mapRow(ResultSet a, int rowA) throws SQLException {
+		Service srvc = new Service();
+		srvc.setServiceID(a.getInt("ServiceID"));
+		srvc.setServiceType(a.getString("Password"));
+		return srvc;
+	}
+	
 }
