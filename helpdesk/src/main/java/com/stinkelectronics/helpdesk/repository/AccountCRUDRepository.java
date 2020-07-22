581f3dbcb4b13d341599e6e27068a407142dad69
Siva <smourougassamy1@student.gsu.edu>
2020-07-22 19:23:06 -0400

    Delete CRUD repository

--- helpdesk/src/main/java/com/stinkelectronics/helpdesk/repository/AccountCRUDRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stinkelectronics.helpdesk.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.stinkelectronics.helpdesk.model.Account;
-
-public interface AccountCRUDRepository extends JpaRepository<Account, Integer>{
-}
