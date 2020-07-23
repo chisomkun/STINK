db511ec2f8636a74cf27fec2292407180b130805
Kevin Nong <Knong1@student.gsu.edu>
2020-07-22 23:41:24 -0400

    WelcomeController.java update

--- helpdesk/src/main/java/com/stinkelectronics/helpdesk/controller/WelcomeController.java
+++ helpdesk/src/main/java/com/stinkelectronics/helpdesk/controller/WelcomeController.java
@@ -1,13 +1,20 @@
 package com.stinkelectronics.helpdesk.controller;
 
+import com.stinkelectronics.helpdesk.model.Profile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.stinkelectronics.helpdesk.service.ProfileDao;
 import com.stinkelectronics.helpdesk.service.RepairDao;
 import com.stinkelectronics.helpdesk.service.RoleDao;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("sessionProfile")
 public class WelcomeController {
 	
 	@Autowired
@@ -20,6 +27,23 @@ public class WelcomeController {
 	private RoleDao roledao;
 	
 	//get welcome
-	
+	@ModelAttribute ("sessionProfile")
+	public Profile makeSessionProfile() {return new Profile();}
+
+
+	@GetMapping
+	public String findingName(Model model, @ModelAttribute("sessionProfile") Profile sessionProfile) {
+
+		model.addAttribute("profile", new Profile());
+		Profile user = new Profile();
+		user.setUserID(sessionProfile.getUserID());
+
+		return "/Welcome";
+
+	}
 	//post welcome
+	@PostMapping
+	public String displayName(Profile sessionProfile){
+		return sessionProfile.getFirstName();
+	}
 }
