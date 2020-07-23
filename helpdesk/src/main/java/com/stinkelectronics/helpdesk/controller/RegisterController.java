f1dda5615792c80bee58d5a3279fdf5bd3b6f887
Siva <smourougassamy1@student.gsu.edu>
2020-07-23 00:37:06 -0400

    Remove unneeded imports

--- helpdesk/src/main/java/com/stinkelectronics/helpdesk/controller/RegisterController.java
+++ helpdesk/src/main/java/com/stinkelectronics/helpdesk/controller/RegisterController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import sun.jvm.hotspot.asm.Register;
 
 
 @Controller
